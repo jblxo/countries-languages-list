85116fc3f72b6a3ea1cfe4518de7a1cc63c8396b
jblxo <hliba.ondra@live.com>
2020-10-29 18:10:15 +0100

    feat(master): create starting next.js, ts and apollo client boilerplate

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { AppProps } from 'next/app';
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache()
+})
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Countries and Languages List</title>
+      </Head>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
